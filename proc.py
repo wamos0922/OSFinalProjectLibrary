11845ff29dec78c49f5063080f917fecfe8f4233
GNB2667 <brandon0812@hanyang.ac.kr>
2025-12-03 10:58:55 +0900

    Pastel_Matte_Template

--- proc.py
+++ proc.py
@@ -33,6 +33,16 @@
 # 2. GRITTY CONTRAST TEMPLATE
 
 # 3. PASTEL MATTE TEMPLATE
+def apply_pastel_matte(img: Image.Image) -> Image.Image:
+    """
+    Applies a 'Soft Pastel Matte' look.
+    Over-brightness and aggressive shadow lift for the matte look.
+    """
+    img = adjust_saturation(img, 1.10)
+    img = adjust_shadows(img, 0.70)
+    img = adjust_brightness(img, 1.20)
+    img = adjust_sharpness(img, 0.90)
+    return img
 
 # --- MANUAL EDIT SEQUENCE ---
 
