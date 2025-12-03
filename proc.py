79810d10bd81f0aef151638c3852a322f61848a3
Wafi <muhammadwafi.wm@gmail.com>
2025-12-03 11:04:23 +0900

    gritty contrast template

--- proc.py
+++ proc.py
@@ -31,6 +31,16 @@
 # 1. GOLDEN HOUR TEMPLATE
 
 # 2. GRITTY CONTRAST TEMPLATE
+def apply_gritty_contrast(img: Image.Image) -> Image.Image:
+    """
+    Applies an 'Urban Gritty Contrast' look.
+    Low saturation and crushed shadows for high contrast.
+    """
+    img = adjust_sharpness(img, 2.50)
+    img = adjust_brightness(img, 0.90)
+    img = adjust_shadows(img, -0.20)
+    img = adjust_saturation(img, 0.80) 
+    return img
 
 # 3. PASTEL MATTE TEMPLATE
 
