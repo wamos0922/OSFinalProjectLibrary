aa8710f64b9d242dd3750e9ae62e7ba2e113be17
Wafi <muhammadwafi.wm@gmail.com>
2025-12-03 11:02:10 +0900

    saturation function

--- proc.py
+++ proc.py
@@ -12,9 +12,17 @@
 
 
 # 2. SATURATION
-
-
-
+def adjust_saturation(img: Image.Image, factor: float) -> Image.Image:
+    """
+    Adjusts the intensity of colors (saturation).
+    
+    Factor 1.0 = original, 0.0 = grayscale, > 1.0 = more vibrant colors.
+    """
+    if not isinstance(img, Image.Image):
+        raise TypeError("Input must be a PIL Image object.")
+        
+    enhancer = ImageEnhance.Color(img)
+    return enhancer.enhance(factor)
 
 # 3. SHARPNESS
 
